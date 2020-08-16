9915803cd2271fcef36e64830cbd021eea06553f
tomj <sunllen2014@outlook.com>
2020-08-16 16:05:47 +0800

    upload hello.c readme.md

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    printf("%d\n", a + b);
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is my first readme file and my repo contains only 1 hello.c file.
+"Hello World!"
